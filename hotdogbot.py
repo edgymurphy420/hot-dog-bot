f82f04339d9dfaa475c1c08f73f678be5430db9c
stinkletoe <lolstankers@gmail.com>
2021-12-12 17:23:56 -0500

    jingles will now remind users who they are

--- hotdogbot.py
+++ hotdogbot.py
@@ -96,5 +96,11 @@ async def on_message(message):
     if 'cringe' in message.content.lower():
         await message.channel.send(f"{random.randint(0, 100)}% cringe")
 
+    # grab retard role id
+    role = message.guild.get_role(919700088543137822)
+    if role in message.author.roles:
+        # if author is a confirmed retard, roll .1 chance jingles will remind him
+        if math.floor(random.random()*3) == 1:
+            await message.channel.send(f"<@{message.author.id}> jingles says you're a fucking retard", file=discord.File('./assets/jingles.jpg'))
 
 client.run(TOKEN)
